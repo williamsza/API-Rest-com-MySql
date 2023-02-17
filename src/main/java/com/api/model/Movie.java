8b4a3ad5b2ac09e51f617a0dcfbce32ee1ff25d0
William de Souza <william.sza@outlook.com>
2023-02-17 14:28:39 -0300

    Configuração e conexão com o banco de dados meySql

---
+++ src/main/java/com/api/model/Movie.java
@@ -0,0 +1,32 @@
+package com.api.model;
+
+public class Movie {
+
+    private String id;
+    private String nome;
+    private boolean active;
+
+    
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public boolean isActive() {
+        return active;
+    }
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    
+
+    
+}
