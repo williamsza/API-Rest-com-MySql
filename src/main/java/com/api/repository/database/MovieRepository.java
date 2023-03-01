fd0593bb9097a64bb62bc083eb710a3296eee4ad
William de Souza <william.sza@outlook.com>
2023-03-01 07:34:07 -0300

    ajustes

--- src/main/java/com/api/repository/database/MovieRepository.java
+++ src/main/java/com/api/repository/database/MovieRepository.java
@@ -1,12 +1,10 @@
 package com.api.repository.database;
-
 import com.api.model.MovieModel;
 
 
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
